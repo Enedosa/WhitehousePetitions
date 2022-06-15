5b22540d0a93ad7e539bc0477e8e425240f53b0f
Ebele <enedosa@yahoo.com>
2022-06-15 23:59:38 +0100

    added detailViewController

---
+++ Seven/Seven/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Seven
+//
+//  Created by Decagon on 15/06/2022.
+//
+
+import Foundation
+import WebKit
+
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
