5c02461792c2dc8481658ee2ef6097047089e36a
Ebele <enedosa@yahoo.com>
2022-06-17 23:47:28 +0100

    added DetailViewController

--- Seven/Seven/DetailViewController.swift
+++ Seven/Seven/DetailViewController.swift
@@ -11,9 +11,29 @@ import WebKit
 
 class DetailViewController: UIViewController {
     var webView: WKWebView!
+    var detailItem: Petition?
     
+    override func loadView() {
+        webView = WKWebView()
+        view =  webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let detailItem = detailItem else {
+            return
+        }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
     }
 }
